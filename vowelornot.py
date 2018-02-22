b2d6f68ea209adb2c73b114168e7f36cfeaa81da
SOPHIA AGNUS H <hendrysophia003@gmail.com>
2018-02-22 19:27:21 +0530

    Create vowelornot.py

---
+++ vowelornot.py
@@ -0,0 +1,5 @@
+a=str(input())
+    if(((a=='a')or(a=='e')or(a=='i')or(a=='o')or(a=='u'))or((a=='A')or(a=='E')or(a=='I')or(a=='O')or(a=='U'))):
+        print("vowel")
+    else:
+        print("consonant")
