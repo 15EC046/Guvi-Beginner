40d4519a233b1a4f12e561db0093b757c7768fba
SOPHIA AGNUS H <hendrysophia003@gmail.com>
2018-02-23 11:29:02 +0530

    Create armstrong number.py

---
+++ armstrong number.py
@@ -0,0 +1,11 @@
+a=int(input())
+    temp=a
+    su=0
+    while(temp>0):
+        r=temp%10
+        su+=r**3
+        temp//=10
+    if(a==su):
+        print("yes")
+    else:
+        print("no")
